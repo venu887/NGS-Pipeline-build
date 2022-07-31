aa8bbaefa692dd59dd56890031a90e0ffd0d5948
venu887 <81346334+venu887@users.noreply.github.com>
2022-07-31 14:39:51 +0800

    Update NGS_pipeline.sh

--- NGS_pipeline.sh
+++ NGS_pipeline.sh
@@ -1,43 +1,53 @@
 SECONDS=0
+
 # Seconds will give us output time for entire run or single run 
 # change working directory
 
 # type pwd to know the root for currient directory
 cd /home/venu/Desktop/NGS_pipeline/
+# 1. In this pipeline I have installed SRA_toolkit then downloaded SRA fastq files(SRR15852393_1.fastq)
+# 2. I used this file for further analysis quality control by FsatQC tool
 
 #Next directory should be Script directory 
 # We need to specify data file which holds fastaq files as input and output
 
-# STEP 1: Run fastqc
-#fastqc data/demo.fastq -o data/SR 
+# Run fastqc
 #SRA_fastaq files
-fastqc data/SRR_1.fastq -o data/
+# Output as .html and .gz files
+#fastqc data/SRR_1.fastq -o data/
 
-# run trimmomatic to trim reads with poor quality
+
+# 3. Run trimmomatic to trim reads with poor quality if necessory after verifying the data for many factors like per base quality, adopters...
+# Sudo apt-get update 
+# Sudo apt-get install Trimmomatic (update again) 
 # There are many tools for trimming data.
-java -jar ~/Desktop/demo/tools/Trimmomatic-0.39/trimmomatic-0.39.jar SE -threads 4 data/demo.fastq data/demo_trimmed.fastq TRAILING:10 -phred33
-echo "Trimmomatic finished running!"
+# Make sure you have to give correct present working directory path and input put file name, Create output file name
+# Out put as FASTQC file
+#java -jar ~//Desktop/NGS_pipeline/Trimmomatic-0.39/trimmomatic-0.39.jar SE -threads 4 data/SRR_1.fastq data/SRR1_Trim.fastq TRAILING:10 -phred33
+#echo "Trimmomatic finished running!"
 
-#fastqc data/demo_trimmed.fastq -o data/
+# If necessory again run FastaQC for new trimmed file
+#fastqc data/SRR1_Trim.fastq -o data/
 
 
-# STEP 2: Run HISAT2
+# 4. Run HISAT2 for genome allignment with humangenome 38
 # HISAT2 proved that very fast than compared wirh STAR 
 # install and run python code provided by HISAT2 
+# Sudo apt-get update 
+# Sudo apt-get install hisat2
 # get the genome indices alignmenr wirh human genome
-# htps://genome-idx.s3.amazonaws.com/hisat/grch38_genome.tar.gz
-
-#
-# run alignment
-hisat2 -q --rna-strandness R -x HISAT2/grch38/genome -U data/SRR_trimmed.fastq | samtools sort -o HISAT2/demo_trimmed.bam
+# wget https://genome-idx.s3.amazonaws.com/hisat/grch38_genome.tar.gz
+# Or  DOWNLOD ditectly http://daehwankimlab.github.io/hisat2/download/#version-hisat2-221https://ccb.jhu.edu/software/hisat2/manual.shtml#getting-started-with-hisat2
+# Extract files and keep it in folder "hisat" in that sub folder called genome has around 8 files
+hisat2 -q --rna-strandness R -x hisat2/grch38/genome -U data/SRR1_Trim.fastq | samtools sort -o hisat2/SRR1_trimmed.bam
 echo "HISAT2 finished running!"
 
 # STEP 3: Run featureCounts - Quantification
 
 # get features 
 # wget http://ftp.ensembl.org/pub/release-106/gtf/homo_sapiens/Homo_sapiens.GRCh38.106.gtf.gz
-featureCounts -S 2 -a ../hg38/Homo_sapiens.GRCh38.106.gtf -o quants/demo_featurecounts.txt HISAT2/demo_trimmed.bam
-echo "featureCounts finished running!"
+#featureCounts -S 2 -a ../hg38/Homo_sapiens.GRCh38.106.gtf -o quants/demo_featurecounts.txt HISAT2/demo_trimmed.bam
+#echo "featureCounts finished running!"
 
 
 duration=$SECONDS
